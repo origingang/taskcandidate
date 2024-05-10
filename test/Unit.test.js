9892c5c4e0d70b59a534d75652fb364fb3113a20
Maxwell Pereira Ferreira <maxwell.ferreira@extractta.com.br>
2024-05-10 16:03:28 -0300

    outro teste aleatorio

---
+++ test/Unit.test.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const request = require("supertest")
+const app = require("../src/myApp.js")
+
+describe("GET /index.html", () => {
+    it("should return 200", async () => {
+        return request(app)
+            .get("/index.html")
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((res) => {
+                expect(res.statusCode).toBe(200);
+            })
+    });
+});
